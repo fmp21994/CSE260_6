777d3f9ca7b6a365c1fe724b294ac114eebb6735
Frank Palmisano <fmp21994@Franks-MacBook-Pro.local>
2015-02-21 17:45:22 -0700

    Initial Commit

---
+++ Assignment6/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Assignment6
+//
+//  Created by Frank Palmisano on 2/21/15.
+//  Copyright (c) 2015 Frank Palmisano. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
